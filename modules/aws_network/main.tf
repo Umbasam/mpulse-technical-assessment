9b71d345bdb008ca2134a7dcd3944f1c23a98939
Jeremy Bergstrom <jbergstrom@optimataxrelief.com>
2022-04-29 00:37:04 -0700

    Initial outline for vpc module using OS module

---
+++ modules/aws_network/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.name
+  cidr = "10.42.0.0/16"
+
+  azs              = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets  = ["10.42.1.0/24", "10.42.2.0/24", "10.42.3.0/24"]
+  public_subnets   = ["10.42.11.0/24", "10.42.12.0/24", "10.42.13.0/24"]
+  database_subnets = ["10.42.21.0/24", "10.42.22.0/24", "10.42.23.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  tags = var.default_tags
+}
---
+++ modules/aws_network/variables.tf
@@ -0,0 +1,5 @@
+variable "name" {
+  default     = "aws_network"
+  description = "The namespace for the created resources"
+  type        = string
+}
