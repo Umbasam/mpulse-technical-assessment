0cc996515252fb6c32329180c4286b40438c6f1c
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-01 23:29:04 -0700

    added az ref from aws_network

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "aws_network" {
   source = "./modules/aws_network"
 }
+
+module "aws_resources" {
+  source = "./modules/aws_resources"
+
+  availability_zone_one = module.aws_network.availability_zone_one
+}
--- modules/aws_resources/main.tf
+++ modules/aws_resources/main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.region
+}
+
+# * All instances should use the latest Amazon Linux 2 AMI
+# * Create a t2.micro EC2 instance in the private subnets
+
+# * Create a t2.micro EC2 instance in the public subnets that you can use as a bastion host to access the private hosts.
+#   * Be ready to show or explain how you would use this host to connect to the private hosts
+
+resource "aws_ec2_host" "private_ec2" {
+  availability_zone = "value" # Need to add output from aws_network
+
+}
