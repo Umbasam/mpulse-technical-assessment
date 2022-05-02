f1f4018850ae4853e6fb4b45954873aef7efac71
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-02 00:01:37 -0700

    added ami logic and subnet var

--- modules/aws_resources/main.tf
+++ modules/aws_resources/main.tf
@@ -16,8 +16,19 @@ provider "aws" {
 
 # * Create a t2.micro EC2 instance in the public subnets that you can use as a bastion host to access the private hosts.
 #   * Be ready to show or explain how you would use this host to connect to the private hosts
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
 
-resource "aws_ec2_host" "private_ec2" {
-  availability_zone = var.availability_zone_one
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+}
+
+resource "aws_instance" "ec2_private" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  subnet_id     = var.private_subnet
 
 }
--- modules/aws_resources/variables.tf
+++ modules/aws_resources/variables.tf
@@ -1,3 +1,11 @@
-variable "availability_zone_one" {
-  description = "The AZ that this module deploys into"
+variable "public_subnet" {
+  description = "The subnet used for publicly accessible resources"
+}
+
+variable "private_subnet" {
+  description = "The subnet used for resources that do not need direct in-bound access and are not databases"
+}
+
+variable "database_subnet" {
+  description = "The subnet used for database resources, which do not need direct in-bound access"
 }
