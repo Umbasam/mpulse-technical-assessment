3dc4551e1e8a87e0bd1825de938dff126991e3f9
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-02 01:18:39 -0700

    Added inbound_ssh and pubkey vars

--- main.tf
+++ main.tf
@@ -5,8 +5,10 @@ module "aws_network" {
 module "aws_resources" {
   source = "./modules/aws_resources"
 
-  vpc_id          = module.aws_network.vpc_id
-  public_subnet   = module.aws_network.public_subnet
-  private_subnet  = module.aws_network.private_subnet
-  database_subnet = module.aws_network.database_subnet
+  vpc_id              = module.aws_network.vpc_id
+  public_subnet       = module.aws_network.public_subnet
+  private_subnet      = module.aws_network.private_subnet
+  database_subnet     = module.aws_network.database_subnet
+  deployer_public_key = var.deployer_public_key
+  inbound_ssh_ip      = var.inbound_ssh_ip
 }
