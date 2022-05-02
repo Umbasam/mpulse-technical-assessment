12a4dc4b2e2ab2611a0785c5c3e7c9847c9993c3
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-02 00:15:31 -0700

    added headers to sections

--- modules/aws_resources/main.tf
+++ modules/aws_resources/main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 
 # * Create a t2.micro EC2 instance in the public subnets that you can use as a bastion host to access the private hosts.
 #   * Be ready to show or explain how you would use this host to connect to the private hosts
+
+# Pulls latest Amazon Linux 2 AMI
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
@@ -26,12 +28,14 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
+# Private EC2 Instance
 resource "aws_instance" "ec2_private" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   subnet_id     = var.private_subnet
 }
 
+# Public EC2 Instance
 resource "aws_security_group" "sg_ec2_public" {
   name        = "sg_ec2_public"
   description = "Allow SSH inbound from specified IP"
