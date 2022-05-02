b704c19a3dde265f7c26271a8891158f76757b0e
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-02 00:32:35 -0700

    added region and re-org'd vars

--- modules/aws_resources/variables.tf
+++ modules/aws_resources/variables.tf
@@ -1,3 +1,19 @@
+variable "region" {
+  default     = "us-west-2"
+  description = "The AWS region that resources will be deployed into"
+  type        = string
+}
+
+variable "inbound_ssh_ip" {
+  description = "This IP, expressed in CIDR notation, will be allowed SSH traffic into the ec2_public bastion host"
+  type        = string
+}
+
+variable "deployer_public_key" {
+  description = "RSA format public key for the keypair used by the EC2 instances"
+}
+
+# Subnet definitions
 variable "public_subnet" {
   description = "The subnet used for publicly accessible resources"
 }
@@ -8,13 +24,4 @@ variable "private_subnet" {
 
 variable "database_subnet" {
   description = "The subnet used for database resources, which do not need direct in-bound access"
-}
-
-variable "inbound_ssh_ip" {
-  description = "This IP, expressed in CIDR notation, will be allowed SSH traffic into the ec2_public bastion host"
-  type        = string
-}
-
-variable "deployer_public_key" {
-  description = "RSA format public key for the keypair used by the EC2 instances"
 }
