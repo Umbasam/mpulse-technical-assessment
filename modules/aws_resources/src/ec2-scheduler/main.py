3c2c9685b91853cc9efcab6a882871682a691ff4
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-03 00:44:33 -0700

    adding better log output to function

--- modules/aws_resources/src/ec2-scheduler/main.py
+++ modules/aws_resources/src/ec2-scheduler/main.py
@@ -6,10 +6,15 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     # Need to capture 2 things: instance id, action (stop/start)
     schedule_action = os.environ['SCHEDULER_ACTION']
+    print(schedule_action)
+    
     instance_id = os.environ['INSTANCE_ID']
+    print(instance_id)
 
     if schedule_action == "ec2_stop":
-        ec2.stop_instances(InstanceIds=[instance_id])            
+        response = ec2.stop_instances(InstanceIds=[instance_id])
+        print(response)     
 
     elif schedule_action == "ec2_start":
-        ec2.start_instances(InstanceIds=[instance_id])
+        response = ec2.start_instances(InstanceIds=[instance_id])
+        print(response)
