d61c4218f0ef7b03e5683605114b6a87a165592e
Jeremy Bergstrom <bergie10@gmail.com>
2022-05-02 23:50:16 -0700

    wrote initial python function

---
+++ modules/aws_resources/src/ec2-scheduler/main.py
@@ -0,0 +1,15 @@
+import os
+import boto3
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    # Need to capture 2 things: instance id, action (stop/start)
+    schedule_action = os.environ['SCHEDULER_ACTION']
+    instance_id = os.environ['INSTANCE_ID']
+
+    if schedule_action == "stop":
+        ec2.stop_instances(InstanceIds=[instance_id])            
+
+    elif schedule_action == "start":
+        ec2.start_instances(InstanceIds=[instance_id])
